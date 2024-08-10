7144b765acb34844a0d3a5b2208eba0f908975c6
JPThrives <boruahjp1999@gmail.com>
2024-08-10 20:46:55 +0530

    Created a new function to generate coustom grids of different sizes which user can chose

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let container = document.querySelector(".container");
+/* let container = document.querySelector(".container");
 
 function makeDivs(num) {
     for(let i = 0; i < num*num ; i++) {
@@ -11,7 +11,36 @@ function makeDivs(num) {
 
         cells.addEventListener("mouseout", event => {
             event.target.style.backgroundColor = "#a3a0a0";
-        })
+        });
     }
 }
-makeDivs(16);
+makeDivs(16); */
+
+let btn = document.querySelector('#generate-grid');
+btn.addEventListener("click", () => {
+    generateGrid();
+});
+
+
+function generateGrid() {
+    let container = document.querySelector('.container');
+    let gridSize = document.querySelector('#grid-size').value;
+    container.innerHTML = '';
+
+    let numCells = gridSize * gridSize;
+    let flexBasis = `calc(100% / ${gridSize})`;
+
+    for(let i = 0; i < numCells; i++) {
+        let cell = document.createElement('div');
+        cell.style.flexBasis = flexBasis;
+        container.appendChild(cell);
+
+        cell.addEventListener("mouseover", event => {
+            event.target.style.backgroundColor = "yellow";
+        });
+
+    }
+}
+
+generateGrid();
+
